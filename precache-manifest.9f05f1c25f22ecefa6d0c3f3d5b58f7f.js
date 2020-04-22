9312b30103ec95ba38b8326796b34079841c2197
Kostya <oliinyk.kostya@gmail.com>
2020-04-22 16:43:34 +0300

    Updates

---
+++ precache-manifest.9f05f1c25f22ecefa6d0c3f3d5b58f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31df02afaa397b0bfae448550f41f9eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2abde9e08b594b817607",
+    "url": "./static/css/main.6cacaf76.chunk.css"
+  },
+  {
+    "revision": "b23cd99607aafc7c9c87",
+    "url": "./static/js/2.bc76db86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc76db86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2abde9e08b594b817607",
+    "url": "./static/js/main.5160eb11.chunk.js"
+  },
+  {
+    "revision": "d34856cc17b978b4a115",
+    "url": "./static/js/runtime-main.9d7a78c6.js"
+  }
+]);
